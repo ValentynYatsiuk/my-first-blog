8fd176f76a35a74280a77532e17f15d28a4e4ba0
ValentynYatsiuk <iatsiuk_ak16@nuwm.edu.ua>
2018-10-18 09:06:45 +0000

    fixed after settings

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,14 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 
-from django.conf.urls import include, url
+from django.conf.urls import  url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+
+  # url(r'^admin/', include(admin.site.urls)),
+  #old and wrong syntax
+
+    url(r'^admin/', admin.site.urls),
     url(r'', include('blog.urls')),
-
-
-
 ]
 
